29ccd2a77b96040433a8aac29679acbc8e36ced4
adityach0uhan <m4with6x@gmail.com>
2024-01-01 15:24:42 +0530

    Added Light & Dark theme

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from 'next/link';
 import Image from 'next/image';
+import { ModeToggle } from '@/components/ModeToggler';
 const Header = () => {
   return (
-    <header className='flex items-center justify-between w-screen h-24 bg-[#]'>
-      <Link className='flex gap-4 items-center h-full w-screen pl-5' href={'/'}>
-        <Image src={""} alt='' className='' height={70} width={90} />
-        <div className='text-white'>Free Cloud</div>
+    <header className='flex items-center justify-between w-screen h-20 border-b-2 border-black'>
+      <Link className=' w-screen pl-4' href={'/'}>
+        <Image src={"/header_image.png"} alt='' className='' height={50} width={80} />
       </Link>
-      <div className='pr-6 text-'>
-
+      <div className='pr-6  flex justify-center gap-2 items-center'>
+        <ModeToggle />
         <UserButton afterSignOutUrl="/" />
         <SignedOut >
           <SignInButton afterSignInUrl='/dashboard' mode='modal' />
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from "@/components/theme-provider"
 import { ClerkProvider } from '@clerk/nextjs'
 import Header from './components/Header'
 const inter = Inter({ subsets: ['latin'] })
@@ -17,10 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
+      <html lang="en">
         <body className={inter.className}>
-          <Header/>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
